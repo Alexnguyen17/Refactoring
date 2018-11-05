c0427c3bc04eba2360c8137c58f2db712bc4ee19
Alexnguyen17 <Alexnguyen170395@gmail.com>
2018-11-05 17:24:52 +0700

    kkk

---
+++ src/Cylinder.php
@@ -0,0 +1,28 @@
+<?php
+
+class Cylinder
+{
+    public function getVolume($radius, $height)
+    {
+        $volume = $this->getPerimeter($radius) * $height + 2 * $this->getBaseArea($radius);
+        return $volume;
+    }
+    /**
+     * @param $radius
+     * @return float|int
+     */
+    public function getBaseArea($radius)
+    {
+        $baseArea = pi() * $radius * $radius;
+        return $baseArea;
+    }
+    /**
+     * @param $radius
+     * @return float|int
+     */
+    public function getPerimeter($radius)
+    {
+        $perimeter = 2 * pi() * $radius;
+        return $perimeter;
+    }
+}
